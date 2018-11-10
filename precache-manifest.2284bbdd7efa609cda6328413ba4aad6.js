8bea566cc17fa5d70c83733f2a4964b1e7d3d116
Rosny Fletez Jr <rfletezjr@gmail.com>
2018-11-10 10:40:59 -0700

    Updates

---
+++ precache-manifest.2284bbdd7efa609cda6328413ba4aad6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chat-client-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "92b903820fe3a13f9417",
+    "url": "/chat-client-react/static/js/runtime~main.92b90382.js"
+  },
+  {
+    "revision": "7a58d707c6033d38f0d4",
+    "url": "/chat-client-react/static/js/main.7a58d707.chunk.js"
+  },
+  {
+    "revision": "c3789b3695e522ab5140",
+    "url": "/chat-client-react/static/js/1.c3789b36.chunk.js"
+  },
+  {
+    "revision": "7a58d707c6033d38f0d4",
+    "url": "/chat-client-react/static/css/main.911541e1.chunk.css"
+  },
+  {
+    "revision": "350042e2ca4cbcf3015ec1d736d7c317",
+    "url": "/chat-client-react/index.html"
+  }
+];
